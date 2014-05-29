30037ed505aa6df20f151446b6eee93cf5ffeea7
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-05-29 14:00:00 -0400

    Create Route.java

---
+++ Route.java
@@ -0,0 +1,23 @@
+public class Route {
+
+private String _origin;
+private String _dest;
+private RouteList master = new RouteList("routes.dat");
+
+public Route(String origin, String dest) { // add verification of codes
+_origin = origin;
+_dest = dest;
+}
+
+public ArrayList<Integer> findDirect() {
+ArrayList<Integer> directs = new ArrayList<Integer>();
+
+
+
+
+
+
+
+}
+
+}
