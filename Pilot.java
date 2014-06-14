3a3a29ecd0e1a3527b4dc05708aa89dc2555f5c4
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-06-14 19:48:46 -0400

    Update Pilot.java

--- Pilot.java
+++ Pilot.java
@@ -22,44 +22,54 @@ public class Pilot {
 
 
 	if (flight.anyDirects()) {
-	    System.out.println("Direct route(s) found. Here are the airlines that fly directly from " + Loc1 + " to " + Loc2 + " enter the number corresponding to your desired airline:");
-	    System.out.println( flight.printDirect());
+	    System.out.println("Direct route(s) found. Here are the airlines that fly directly from " + Loc1 + " to " + Loc2 + ".");
+	    System.out.println( flight.printDirects());
 	    String routechoice;
-	    System.out.println("Please input the number of the flight you'd like to take: ");
-	routechoice = user_input.next();
+	    System.out.println("Enter the number corresponding to your desired airline:");
+	    routechoice = user_input.next();
 
 	}
+
 	else {
-	    System.out.println("Searching for transfer locations...");
-	    //print list
-	    String transferchoice;
-	    System.out.println("Please input the number of the flight you'd like to take: ");
-	transferchoice = user_input.next();
+	    System.out.println("No direct route found. Searching for transfer location(s)...");
+	    flight.findTransfer();
+
+	    if (flight.anyTransfers()) {
+	    System.out.println("Transfer location(s) found. Here are the layover cities between " + Loc1 + " and " + Loc2 + ".");
+		System.out.println( flight.printTransfers());
+		String transferchoice;
+		System.out.println("Enter the number corresponding to your desired layover city:");
+		transferchoice = user_input.next();
 	
 	
 	
 	
-		    System.out.println("Searching for airlines for transfer locationsthe first leg of your trip...");
-	     flight = new Route(Loc1,transferchoice);
-	     flight.findDirect();
-	     System.out.println(flight.printDirects());
-	     String leg1;
-	    System.out.println("Please pick an airline for the first leg of your trip:");
-	    leg1 = user_input.next();
+		System.out.println("Searching for airlines for transfer locationsthe first leg of your trip...");
+		flight = new Route(Loc1,transferchoice);
+		flight.findDirect();
+		System.out.println(flight.printDirects());
+		String leg1;
+		System.out.println("Please pick an airline for the first leg of your trip:");
+		leg1 = user_input.next();
 	
+	    }
+
+	    else {
+		System.out.println("No flights exist");
+	    }
 	}
 	
-	String date;
-	System.out.println("Please input the date of your trip: ");
-	date = user_input.next();
+	    String date;
+	    System.out.println("Please input the date of your trip: ");
+	    date = user_input.next();
 
-	//determine price
-	//create seating chart, print seating chart
+	    //determine price
+	    //create seating chart, print seating chart
+
+
+
+	}
 
 
 
     }
-
-
-
-}
