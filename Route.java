e4b690bd66a911ea76ce0f8b65db7c7db2ea55cb
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-06-15 21:50:14 -0400

    Update Route.java

--- Route.java
+++ Route.java
@@ -3,8 +3,13 @@ import java.util.*;
 public class Route {
 
     private String _origin;
+    private double _lat1;
+    private double _long1;
     private String _dest;
-    private String _transfer;
+    private double _lat2;
+    private double _long2;
+
+    private double distance;
     private ArrayList cities = new ArrayList();
 
     private ListMaker master = new ListMaker("routes.dat", 59637, 8);
@@ -22,15 +27,53 @@ public class Route {
 	_dest = dest;
     }
 
+    public double calcDistance() {
+	for (int a = 0; a < airports.length; a++) {
+	    if (_origin.equals(airports[a][4])) {
+		_lat1 = Double.parseDouble(airports[a][6]);
+		_long1 = Double.parseDouble(airports[a][7]);
+	    }
+	    if (_dest.equals(airports[a][4])) {
+		_lat2 = Double.parseDouble(airports[a][6]);
+		_long2 = Double.parseDouble(airports[a][7]);
+	    }
+	}
+	distance = haversine(_lat1, _long1, _lat2, _long2);
+	return distance;
+    }
+
+    public static double haversine(
+				   double lat1, double lng1, double lat2, double lng2) {
+	int r = 6371; // average radius of the earth in km
+	double dLat = Math.toRadians(lat2 - lat1);
+	double dLon = Math.toRadians(lng2 - lng1);
+	double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	    Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) 
+	    * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+	double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	double d = r * c;
+	return d;
+    }
+
     public ArrayList<Integer> findDirect() {
 	for(int r = 0; r < routes.length; r++)
 	    if ((routes[r][2].equals(_origin) && routes[r][4].equals(_dest))) 
 		directs.add(r);
+
+	HashSet hs = new HashSet();
+	hs.addAll(directs);
+	directs.clear();
+	directs.addAll(hs);
+
 	return directs;
     }
 
     public ArrayList getCities() {
 	return cities;
+    }
+
+    public ArrayList<Integer> getDirects() {
+	return directs;
     }
 
     public boolean anyDirects() {
@@ -43,7 +86,7 @@ public class Route {
 
     public String printDirects() {
 	String ret = "";
-	for(int i = 0; i < directs.size()/2; i++) {
+	for(int i = 0; i < directs.size(); i++) {
 	    for(int j = 0; j < airlines.length; j++) {
 		if (routes[directs.get(i)][0].equals(airlines[j][3])) {
 		    ret += i + ": " + airlines[j][1] + "\n";
@@ -56,7 +99,7 @@ public class Route {
 
     public String printTransfers() {
 	String ret = "";
-	for(int i = 0; i < transfers.size()/2; i++)
+	for(int i = 0; i < transfers.size(); i++)
 	    cities.add(transfers.get(i));
 
 	HashSet hs = new HashSet();
@@ -74,15 +117,6 @@ public class Route {
 	    }
 	}
 	return ret;
-
-	// for(int i = 0; i < transfers.size()/2; i++) {
-	//     for (int j = 0; j < cities.size(); j++) {
-	// 	if (transfers.get(i).equals(cities.get(j)))
-	// 	    break;
-	// 	cities.add(transfers.get(i));
-	// 	System.out.println(transfers.get(i));
-	//     }
-	// }
 
     }
 
