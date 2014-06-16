6d1e1c5672bc5d1ea3c951dc8041f672e2be22fe
Justin Strauss <justinstrauss@users.noreply.github.com>
2014-06-15 21:50:38 -0400

    Update Pilot.java

--- Pilot.java
+++ Pilot.java
@@ -1,4 +1,6 @@
 import java.util.*;
+//import org.joda.time.DateTime;
+//import org.joda.time.Days;
 
 public class Pilot {
 
@@ -24,9 +26,21 @@ public class Pilot {
 	if (flight.anyDirects()) {
 	    System.out.println("Direct route(s) found. Here are the airlines that fly directly from " + Loc1 + " to " + Loc2 + ".");
 	    System.out.println( flight.printDirects());
-	    String routechoice;
+	    int routechoice = -1;
 	    System.out.println("Enter the number corresponding to your desired airline:");
-	    routechoice = user_input.next();
+	    String tmp = user_input.next().trim();
+	    try {
+		routechoice = Integer.parseInt(tmp);
+	    }
+	    catch (Exception e) {
+		System.out.println("Sorry, your input is not an number. Please try again.");
+		System.exit(1);
+	    }
+
+	    if (routechoice < 0 || routechoice >= flight.getDirects().size()) {
+		System.out.println("Sorry, your input is out of range. Please try again.");
+		System.exit(1);
+	    }
 
 	}
 
@@ -35,49 +49,189 @@ public class Pilot {
 	    flight.findTransfer();
 
 	    if (flight.anyTransfers()) {
-	    System.out.println("Transfer location(s) found. Here are the layover cities between " + Loc1 + " and " + Loc2 + ".");
+		System.out.println("Transfer location(s) found. Here are the layover cities between " + Loc1 + " and " + Loc2 + ".");
 		System.out.println( flight.printTransfers());
 		String transferchoice;
-		System.out.println("Enter the number corresponding to your desired layover city:");
-		int num = Integer.parseInt(user_input.next());
+		int num = -1;
 		ArrayList<String> cities = flight.getCities();
+		System.out.println("Enter the number corresponding to your desired layover city:");
+		try {
+		    num = Integer.parseInt(user_input.next());
+		}
+		catch (Exception e) {
+		    System.out.println("Sorry, your input is not an number. Please try again.");
+		    System.exit(1);
+		}
+
+		if (num < 0 || num >= cities.size()) {
+		    System.out.println("Sorry, your input is out of range. Please try again.");
+		    System.exit(1);
+		}
+		
+		
 		transferchoice = cities.get(num);
 	
-	System.out.println("Here are the airlines that fly directly from " + Loc1 + " to " + transferchoice + ".");
-	Route leg1 = new Route(Loc1,transferchoice);
-	leg1.findDirect();
-	System.out.println( leg1.printDirects());
-	String leg1choice;
-	System.out.println("Enter the number corresponding to your desired airline for the first leg of your trip:");
-	leg1choice = user_input.next();
+		System.out.println("Here are the airlines that fly directly from " + Loc1 + " to " + transferchoice + ".");
+		Route leg1 = new Route(Loc1,transferchoice);
+		leg1.findDirect();
+		System.out.println( leg1.printDirects());
+		int leg1choice = -1;
+		System.out.println("Enter the number corresponding to your desired airline:");
+		String tmp1 = user_input.next().trim();
+		try {
+		    leg1choice = Integer.parseInt(tmp1);
+		}
+		catch (Exception e) {
+		    System.out.println("Sorry, your input is not an number. Please try again.");
+		    System.exit(1);
+		}
+
+		if (leg1choice < 0 || leg1choice >= leg1.getDirects().size()) {
+		    System.out.println("Sorry, your input is out of range. Please try again.");
+		    System.exit(1);
+		}
+
 	
 	
-	System.out.println("Here are the airlines that fly directly from " + transferchoice + " to " + Loc2 + ".");
-	Route leg2 = new Route(transferchoice, Loc2);
+		System.out.println("Here are the airlines that fly directly from " + transferchoice + " to " + Loc2 + ".");
+		Route leg2 = new Route(transferchoice, Loc2);
 		leg2.findDirect();
 		System.out.println(leg2.printDirects());
-		String leg2choice;
-		System.out.println("Enter the number corresponding to your desired airline for the second leg of your trip:");
-		leg2choice = user_input.next();
-	
+		int leg2choice = -1;
+		System.out.println("Enter the number corresponding to your desired airline:");
+		String tmp2 = user_input.next().trim();
+		try {
+		    leg2choice = Integer.parseInt(tmp2);
+		}
+		catch (Exception e) {
+		    System.out.println("Sorry, your input is not an number. Please try again.");
+		    System.exit(1);
+		}
+
+		if (leg2choice < 0 || leg2choice >= leg2.getDirects().size()) {
+		    System.out.println("Sorry, your input is out of range. Please try again.");
+		    System.exit(1);
+		}
+
 	    }
+	
+	
 
 	    else {
 		System.out.println("Sorry, no flights exist. You may have incorrectly entered the 3 character airport code or travel between your two desired airports may require more than one transfer. Please try again.");
+		System.exit(1);
 	    }
 	}
 	
-	    String date;
-	    System.out.println("Please input the date of your trip: ");
-	    date = user_input.next();
+	Date dateDepart = new Date();
+	System.out.println("Please input the departure date of your trip in yyyy/mm/dd format:");
+	String depart = user_input.next();
 
-	    //determine price
-	    //create seating chart, print seating chart
-
-
-
+	ArrayList<String> arr = new ArrayList<String>();
+	String tmp3 = "";
+	for (int i = 0; i<depart.length(); i++) {
+	    String sym = depart.substring(i,i+1);
+	    if (sym.equals("/")) {
+		arr.add(tmp3);
+		tmp3 = "";
+	    }
+	    else
+		tmp3 += sym;
 	}
+	arr.add(tmp3);
+
+	try {
+	    dateDepart = new Date(Integer.parseInt(arr.get(0)),Integer.parseInt(arr.get(1)),Integer.parseInt(arr.get(2)));
+	}
+	catch (Exception e) {
+	    System.out.println("Sorry, the date you inputted is either incorrectly formatted or doesn't exist. Please try again.");
+	    System.exit(1);
+	}
+
+	Date todaysDate = new Date();
+	Calendar todayCal = new GregorianCalendar();
+	todayCal.setTime(todaysDate);
+
+	System.out.println("Do you wish to book a return flight as well? (yes/no)");
+	String ans = user_input.next();
+
+	if (!(ans.equals("yes")) && !(ans.equals("no")))
+	    System.out.println("Input not recognized. We will assume you only want to book one way.");
+
+	if (ans.equals("yes")) {
+
+	    Date dateReturn = new Date();
+	    System.out.println("Please input the return date of your trip in mm/dd/yyyy format:");
+	    String Return = user_input.next();
+
+	    ArrayList<String> arr1 = new ArrayList<String>();
+	    String tmp4 = "";
+	    for (int i = 0; i<Return.length(); i++) {
+		String sym = Return.substring(i,i+1);
+		if (sym.equals("/")) {
+		    arr1.add(tmp4);
+		    tmp4 = "";
+		}
+		else
+		    tmp4 += sym;
+	    }
+	    arr1.add(tmp4);
+
+	    try {
+		dateReturn = new Date(Integer.parseInt(arr1.get(0)),Integer.parseInt(arr1.get(1)),Integer.parseInt(arr1.get(2)));
+	    }
+	    catch (Exception e) {
+		System.out.println("Sorry, the date you inputted is either incorrectly formatted or doesn't exist. Please try again.");
+		System.exit(1);
+	    }
+
+	    if (todaysDate.after(dateReturn)) {
+		System.out.println("Sorry your return date is in the past. Please try again.");
+		System.exit(3);
+	    }
+
+	    if (dateDepart.after(dateReturn)) {
+		System.out.println("Sorry your departure date is after your return date. We will assume you mixed them up.");
+		Date tempDate = dateDepart;
+		dateDepart = dateReturn;
+		dateReturn = tempDate;
+	    }
+
+	    Calendar returnCal = new GregorianCalendar();
+	    returnCal.setTime(dateReturn);
+	    long daysUntilReturn = daysBetween(todayCal,returnCal) - 693991;
+	    System.out.println("days till return: " + daysUntilReturn);
+	}
+
+	if (todaysDate.after(dateDepart)) {
+	    System.out.println("Sorry your departure date is in the past. Please try again.");
+	    System.exit(3);
+	}
+
+	Calendar departCal = new GregorianCalendar();
+	departCal.setTime(dateDepart);
+	long daysUntilDepart = daysBetween(todayCal,departCal) - 693991;
+	System.out.println("days till depart: " + daysUntilDepart); //
+
+
+	System.out.println("distance: " + flight.calcDistance()); //
+
+	//determine price
+	//create seating chart, print seating chart
 
 
 
     }
+
+    public static long daysBetween(Calendar startDate, Calendar endDate) {  
+	//assert: startDate must be before endDate  
+	Calendar date = (Calendar) startDate.clone();  
+	long daysBetween = 0;  
+	while (date.before(endDate)) {  
+	    date.add(Calendar.DAY_OF_MONTH, 1);  
+	    daysBetween++;  
+	}  
+	return daysBetween;  
+    }  
+
+}
